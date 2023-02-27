55f69f1e9d8696f7b5daf1b1a4391fda2b6ae49b
enterus01 <49946174+enterus01@users.noreply.github.com>
2023-02-27 22:41:51 +0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -46,7 +46,7 @@ def uploadToBlobStorage(file_path,file_name):
    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
    blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
    with open(file_path,'rb') as data:
-      blob_client.upload_blob(data)
+      blob_client.upload_blob(data,overwrite=True)
       print(f'Uploaded {file_name}.')
 
 # calling a function to perform upload
