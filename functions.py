ea10d29ff41582d23ad060957fa9572c2a8d2a83
enterus01 <49946174+enterus01@users.noreply.github.com>
2023-03-28 11:36:15 +0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -44,7 +44,7 @@ def convert_size(size_bytes):
 
 
 def uploadToBlobStorage(file_path,file_name):
-   blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+   blob_service_client = BlobServiceClient.from_connection_string(connection_string ,logging_enable=False)
    blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
    with open(file_path,'rb') as data:
       blob_client.upload_blob(data,overwrite=True)
